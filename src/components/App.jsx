2ca89a709e328aeefae285e1af2bb9e94740f61a
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-01-09 14:17:35 +0200

    delete unused elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,7 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
 import { InputForm } from './InputForm/InputForm';
 import { ContactList } from './ContactList/ContactList';
 
-const startContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 export function App() {
-  // const [contacts, setContacts] = useState(() => {
-  //   if (localStorage.getItem('contacts')) {
-  //     return JSON.parse(localStorage.getItem('contacts'));
-  //   }
-  //   return startContacts;
-  // });
-  // const [filter, setFilter] = useState('');
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // function addContact(newContact, newPhone) {
-  //   if (contacts.find(cont => cont.name === newContact)) {
-  //     alert(`${newContact} is already`);
-  //     return;
-  //   }
-  //   setContacts([
-  //     ...contacts,
-  //     { id: nanoid(), name: newContact, number: newPhone },
-  //   ]);
-  // }
-  function deleteContact(input) {
-    // const updatedContacts = contacts.filter(
-    //   cont => input.target.id !== cont.id
-    // );
-    // setContacts(updatedContacts);
-  }
-
   return (
     <section>
       <h1>Phonebook</h1>
